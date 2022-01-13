50d5dbb25b9e09fb220f62a1530e9eb75a1aa45f
iman67 <eng.iansari@gmail.com>
2022-01-14 01:29:25 +0330

    Upload code

---
+++ PolynomialRegression.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+%matplotlib inline
+
+!wget -O FuelConsumption.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv
+
+df = pd.read_csv("FuelConsumption.csv")
+
+# take a look at the dataset
+df.head()
+
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf.head(9)
+
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+
+
+poly = PolynomialFeatures(degree=2)
+train_x_poly = poly.fit_transform(train_x)
+train_x_poly
+
+clf = linear_model.LinearRegression()
+train_y_ = clf.fit(train_x_poly, train_y)
+# The coefficients
+print ('Coefficients: ', clf.coef_)
+print ('Intercept: ',clf.intercept_)
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+XX = np.arange(0.0, 10.0, 0.1)
+yy = clf.intercept_[0]+ clf.coef_[0][1]*XX+ clf.coef_[0][2]*np.power(XX, 2)
+plt.plot(XX, yy, '-r' )
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+
+from sklearn.metrics import r2_score
+
+test_x_poly = poly.transform(test_x)
+test_y_ = clf.predict(test_x_poly)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y,test_y_ ) )
+
