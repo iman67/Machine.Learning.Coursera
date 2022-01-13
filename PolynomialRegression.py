e1b5efc57ad87cdc8361c7d2b28968dae4460f18
iman67 <eng.iansari@gmail.com>
2022-01-14 01:35:57 +0330

    Add new codes

--- PolynomialRegression.py
+++ PolynomialRegression.py
@@ -58,3 +58,24 @@ print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
 print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
 print("R2-score: %.2f" % r2_score(test_y,test_y_ ) )
 
+
+poly3 = PolynomialFeatures(degree=3)
+train_x_poly3 = poly3.fit_transform(train_x)
+clf3 = linear_model.LinearRegression()
+train_y3_ = clf3.fit(train_x_poly3, train_y)
+
+# The coefficients
+print ('Coefficients: ', clf3.coef_)
+print ('Intercept: ',clf3.intercept_)
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+XX = np.arange(0.0, 10.0, 0.1)
+yy = clf3.intercept_[0]+ clf3.coef_[0][1]*XX + clf3.coef_[0][2]*np.power(XX, 2) + clf3.coef_[0][3]*np.power(XX, 3)
+plt.plot(XX, yy, '-r' )
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+test_x_poly3 = poly3.transform(test_x)
+test_y3_ = clf3.predict(test_x_poly3)
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y3_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y3_ - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y,test_y3_ ) )
+
