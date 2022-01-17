37efb21c34b4cdc01bed1c812c1197790e66dd61
iman67 <eng.iansari@gmail.com>
2022-01-17 16:18:49 +0330

    upload python code

---
+++ NonLinearRegressionAnalysis.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+x = np.arange(-5.0, 5.0, 0.1)
+
+##You can adjust the slope and intercept to verify the changes in the graph
+y = 2*(x) + 3
+y_noise = 2 * np.random.normal(size=x.size)
+ydata = y + y_noise
+#plt.figure(figsize=(8,6))
+plt.plot(x, ydata,  'bo')
+plt.plot(x,y, 'r') 
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
